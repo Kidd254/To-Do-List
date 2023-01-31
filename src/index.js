67fa44f4c53cca6277947d9d1e5fb02a57a413a7
mastercoder <lorenkioko@gmail.com>
2023-01-31 13:03:14 +0300

    add basic webpack configuration

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import './style.css';
+
+const taskList = document.getElementById('list');
+
+const listArr = [
+  {
+    task: 'complete to do list activity',
+    completed: true,
+    index: 5,
+  },
+  {
+    task: 'Do some freelance',
+    completed: false,
+    index: 2,
+  },
+  {
+    task: 'attend the stand-up meeting',
+    completed: false,
+    index: 3,
+  },
+];
+
+const desplayToPage = (activity) => {
+  const element = document.createElement('li');
+  element.setAttribute('id', activity.index);
+  element.innerHTML = `
+  <input type="checkbox" class = "checkbox" ${activity.completed ? 'checked' : ''}/>
+  <article>${activity.task}</article>`;
+  taskList.appendChild(element);
+};
+
+const loadElements = () => {
+  listArr.sort((x, y) => x.index - y.index)
+    .forEach((activity) => {
+      desplayToPage(activity);
+    });
+};
+
+window.onload = () => {
+  loadElements();
+};
